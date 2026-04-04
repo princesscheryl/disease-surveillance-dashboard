ba472948e4f6b6b640c95d6b08a76bbd407e86b8
AkuaAmofa <144915602+AkuaAmofa@users.noreply.github.com>
2026-04-04 22:39:05 +0000

    fix: add missing settings import in users/models.py

--- disease_surveillance_dashboard/users/models.py
+++ disease_surveillance_dashboard/users/models.py
@@ -1,5 +1,6 @@
 from typing import ClassVar
 
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import CharField
